7d43d4b676db81191b6e76c232cfef1d6e53bdea
Thomas Klosterman <tklos96@gmail.com>
2021-08-14 12:32:15 -0500

    created a test suite bash script

---
+++ run_test_suite.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+python setup.py test -d 1 --build build_1d
+python setup.py test -d 2 --build build_2d
+python setup.py test -d 3 --build build_3d
+
+cd build_1d
+make
+cd ..
+cd build_2d
+make
+cd ..
+cd build_3d
+make
+cd ..
+
+cd build_1d
+make test
+exit_code_1=$?
+cd ..
+cd build_2d
+make test
+exit_code_2=$?
+cd ..
+cd build_3d
+make test
+exit_code_3=$?
+cd ..
+
+if [ $exit_code_1 -eq 0 ] && [ $exit_code_2 -eq 0 ] && [ $exit_code_3 -eq 0 ];
+then
+  echo SUCCESS!
+  rm -r build_1d
+  rm -r build_2d
+  rm -r build_3d
+else
+  if [ $exit_code_1 -ne 0 ];
+  then
+    echo "Error: 1D test failed in execution"
+  fi
+  if [ $exit_code_2 -ne 0 ];
+  then
+    echo "Error: 2D test failed in execution"
+  fi
+  if [ $exit_code_3 -ne 0 ];
+  then
+    echo "Error: 3D test failed in execution"
+  fi
+  echo FAILURE!
+fi
