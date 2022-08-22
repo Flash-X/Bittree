4798b975b32d1397e1acd371c24ea0c8e4f96d12
Thomas Klosterman <tklos96@gmail.com>
2022-08-22 12:55:54 -0500

    adding notice to setup

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
 
+# Copyright 2022 UChicago Argonne, LLC and contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); 
+# you may not use this file except in compliance with the License. 
+#  
+#
+# Unless required by applicable law or agreed to in writing, software 
+# distributed under the License is distributed on an "AS IS" BASIS, 
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+# See the License for the specific language governing permissions and 
+# limitations under the License.
+
+
 # Users should invoke this script with a setup line similar to one of the following:
 # `python setup.py test -d 2`
 # `python setup.py library -d 2`
